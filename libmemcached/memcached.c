2bb5c4d8d0b27380e1f0c1240581ddb592fdb577
Brian Aker <brian@tangent.org>
2008-05-20 18:46:07 -0700

    Found issue with rollback for clone operation with prefix key.

--- libmemcached/memcached.c
+++ libmemcached/memcached.c
@@ -122,7 +122,10 @@ memcached_st *memcached_clone(memcached_st *clone, memcached_st *ptr)
   new_clone->delete_trigger= ptr->delete_trigger;
 
   if (ptr->prefix_key[0] != 0)
+  {
     strcpy(new_clone->prefix_key, ptr->prefix_key);
+    new_clone->prefix_key_length= ptr->prefix_key_length;
+  }
 
   rc= run_distribution(new_clone);
   if (rc != MEMCACHED_SUCCESS)
