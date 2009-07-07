90e59078950cca63d342f5529ddf6f55b8c85771
Monty Taylor <mordred@inaugust.com>
2009-07-07 00:03:17 -0700

    Added an else for unknown compilers.

--- libmemcached/visibility.h
+++ libmemcached/visibility.h
@@ -34,6 +34,9 @@
 # elif defined(_MSC_VER)
 #  define LIBMEMCACHED_API extern __declspec(dllexport) 
 #  define LIBMEMCACHED_LOCAL
+# else
+#  define LIBMEMCACHED_API
+#  define LIBMEMCACHED_LOCAL
 # endif /* defined(HAVE_VISIBILITY) */
 #else  /* defined(BUILDING_LIBMEMCACHED) */
 # if defined(_MSC_VER)
