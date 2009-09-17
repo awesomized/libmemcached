4a1c481fd0e342bc2339a1998c408c318171d392
Trond Norbye <trond.norbye@sun.com>
2009-09-17 14:39:18 +0200

    Refactor: move ntohll to a separate helper library

--- libmemcached/byteorder.c
+++ libmemcached/byteorder.c
@@ -1,14 +1,14 @@
-#include "common.h"
+#include "byteorder.h"
 
 /* Byte swap a 64-bit number. */
-static inline uint64_t swap64(uint64_t in) 
+static inline uint64_t swap64(uint64_t in)
 {
 #ifndef BYTEORDER_BIG_ENDIAN
   /* Little endian, flip the bytes around until someone makes a faster/better
    * way to do this. */
   uint64_t rv= 0;
   uint8_t x= 0;
-  for(x= 0; x < 8; x++) 
+  for(x= 0; x < 8; x++)
   {
     rv= (rv << 8) | (in & 0xff);
     in >>= 8;
