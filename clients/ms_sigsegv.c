97a9ea6fd9e5df4a1c6eea734da2fc77ce399ebc
Trond Norbye <trond.norbye@sun.com>
2009-12-05 14:11:26 +0100

    Created a pandora-check for printing callstacks

--- clients/ms_sigsegv.c
+++ clients/ms_sigsegv.c
@@ -7,6 +7,9 @@
  * (c) Copyright 2009, Schooner Information Technology, Inc.
  * http://www.schoonerinfotech.com/
  *
+ * Rewrite of stack dump:
+ *  Copyright (C) 2009 Sun Microsystems
+ *  Author Trond Norbye
  */
 
 #include "config.h"
@@ -15,30 +18,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <signal.h>
-#include <ucontext.h>
-#include <dlfcn.h>
-#include <execinfo.h>
 #include <pthread.h>
 
 #include "ms_memslap.h"
 #include "ms_setting.h"
-
-#if defined(__cplusplus) && defined(HAVE_ABI_CXA_DEMANGLE)
-# include <cxxabi.h>
-#endif
-
-#undef REG_RIP
-
-#if defined(REG_RIP)
-# define SIGSEGV_STACK_IA64
-# define REGFORMAT    "%016lx"
-#elif defined(REG_EIP)
-# define SIGSEGV_STACK_X86
-# define REGFORMAT    "%08x"
-#else
-# define SIGSEGV_STACK_GENERIC
-# define REGFORMAT    "%x"
-#endif
 
 /* prototypes */
 int ms_setup_sigsegv(void);
@@ -49,83 +32,17 @@ int ms_setup_sigint(void);
 /* signal seg reaches, this function will run */
 static void ms_signal_segv(int signum, siginfo_t *info, void *ptr)
 {
-  int i;
-
   UNUSED_ARGUMENT(signum);
   UNUSED_ARGUMENT(info);
   UNUSED_ARGUMENT(ptr);
 
   pthread_mutex_lock(&ms_global.quit_mutex);
-  fprintf(stderr, "Segmentation fault occurred.\n");
-
-#if defined(SIGSEGV_STACK_X86) || defined(SIGSEGV_STACK_IA64)
-  int f= 0;
-  Dl_info dlinfo;
-  void **bp= 0;
-  void *ip= 0;
-#else
-  void *bt[20];
-  char **strings;
-  int  sz;
-#endif
-
-#if defined(SIGSEGV_STACK_X86) || defined(SIGSEGV_STACK_IA64)
-# if defined(SIGSEGV_STACK_IA64)
-  ip= (void *)ucontext->uc_mcontext.gregs[REG_RIP];
-  bp= (void **)ucontext->uc_mcontext.gregs[REG_RBP];
-# elif defined(SIGSEGV_STACK_X86)
-  ip= (void *)ucontext->uc_mcontext.gregs[REG_EIP];
-  bp= (void **)ucontext->uc_mcontext.gregs[REG_EBP];
-# endif
-
-  fprintf(stderr, "Stack trace:\n");
-  while (bp && ip)
-  {
-    if (! dladdr(ip, &dlinfo))
-      break;
-
-    const char *symname= dlinfo.dli_sname;
-# if defined(HAVE_ABI_CXA_DEMANGLE) && defined(__cplusplus)
-    int status;
-    char *tmp= __cxa_demangle(symname, NULL, 0, &status);
-
-    if ((status == 0) && tmp)
-      symname= tmp;
-# endif
-
-    fprintf(stderr, "% 2d: %p <%s+%u> (%s)\n",
-            ++f,
-            ip,
-            symname,
-            (unsigned)(ip - dlinfo.dli_saddr),
-            dlinfo.dli_fname);
-
-# if defined(HAVE_ABI_CXA_DEMANGLE) && defined(__cplusplus)
-    if (tmp)
-      free(tmp);
-# endif
-
-    if (dlinfo.dli_sname && ! strcmp(dlinfo.dli_sname, "main"))
-      break;
-
-    ip= bp[1];
-    bp= (void **)bp[0];
-  }
-#else
-  fprintf(stderr, "Stack trace:\n");
-  sz= backtrace(bt, 20);
-  strings= backtrace_symbols(bt, sz);
-
-  for (i= 0; i < sz; ++i)
-  {
-    fprintf(stderr, "%s\n", strings[i]);
-  }
-#endif
+  fprintf(stderr, "Segmentation fault occurred.\nStack trace:\n");
+  pandora_print_callstack(stderr);
   fprintf(stderr, "End of stack trace\n");
   pthread_mutex_unlock(&ms_global.quit_mutex);
-  exit(1);
-} /* ms_signal_segv */
-
+  abort();
+}
 
 /* signal pipe reaches, this function will run */
 static void ms_signal_pipe(int signum, siginfo_t *info, void *ptr)
