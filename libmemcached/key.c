95a7277d4ff7e1f592122028fca5f93b490ff438
Brian Aker <brian@gaz>
2010-01-02 12:08:56 -0800

    White space fix

--- libmemcached/key.c
+++ libmemcached/key.c
@@ -1,7 +1,7 @@
 #include "common.h"
 
-memcached_return_t memcached_key_test(const char * const *keys, 
-                                      const size_t *key_length, 
+memcached_return_t memcached_key_test(const char * const *keys,
+                                      const size_t *key_length,
                                       size_t number_of_keys)
 {
   uint32_t x;
@@ -14,7 +14,7 @@ memcached_return_t memcached_key_test(const char * const *keys,
     rc= memcached_validate_key_length(*(key_length + x), false);
     if (rc != MEMCACHED_SUCCESS)
       return rc;
-    
+ 
     for (y= 0; y < *(key_length + x); y++)
     {
       if ((isgraph(keys[x][y])) == 0)
