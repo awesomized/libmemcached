c4e5fc1697e0bc22da0adda38c73cc32d2b40523
Monty Taylor <mordred@inaugust.com>
2010-01-13 18:52:56 -0800

    Fixed build on CentOS.

--- clients/ms_task.c
+++ clients/ms_task.c
@@ -12,6 +12,16 @@
 #include "config.h"
 
 #include <inttypes.h>
+#if TIME_WITH_SYS_TIME
+# include <sys/time.h>
+# include <time.h>
+#else
+# if HAVE_SYS_TIME_H
+#  include <sys/time.h>
+# else
+#  include <time.h>
+# endif
+#endif
 
 #include "ms_thread.h"
 #include "ms_setting.h"
