fabf488ffa687540145ca8973ceb3fb0147a858b
Brian Aker <brian@tangent.org>
2012-02-14 15:27:33 -0800

    Add in a test to do memcached_set() after a kill has been issued.

--- tests/libmemcached-1.0/mem_functions.h
+++ tests/libmemcached-1.0/mem_functions.h
@@ -178,3 +178,4 @@ test_return_t util_version_test(memcached_st *memc);
 test_return_t version_string_test(memcached_st *);
 test_return_t wrong_failure_counter_test(memcached_st *memc);
 test_return_t wrong_failure_counter_two_test(memcached_st *memc);
+test_return_t kill_HUP_TEST(memcached_st *memc);
