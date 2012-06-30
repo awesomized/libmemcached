f6a25ad2e38d85584d0d55b491d63338cb1e73cf
Brian Aker <brian@tangent.org>
2012-06-30 09:47:49 -0700

    First pass in merging libtest

---
+++ libtest/dns.cc
@@ -0,0 +1,97 @@
+/*  vim:expandtab:shiftwidth=2:tabstop=2:smarttab:
+ *
+ *  Data Differential YATL (i.e. libtest)  library
+ *
+ *  Copyright (C) 2012 Data Differential, http://datadifferential.com/
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are
+ *  met:
+ *
+ *      * Redistributions of source code must retain the above copyright
+ *  notice, this list of conditions and the following disclaimer.
+ *
+ *      * Redistributions in binary form must reproduce the above
+ *  copyright notice, this list of conditions and the following disclaimer
+ *  in the documentation and/or other materials provided with the
+ *  distribution.
+ *
+ *      * The names of its contributors may not be used to endorse or
+ *  promote products derived from this software without specific prior
+ *  written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#include <config.h>
+#include <libtest/common.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+namespace libtest {
+
+bool lookup(const char* host)
+{
+  assert(host);
+  bool success= false;
+  struct addrinfo *addrinfo= NULL;
+
+  int limit= 5;
+  while (limit--)
+  {
+    int ret;
+    if ((ret= getaddrinfo(host, NULL, NULL, &(addrinfo))))
+    {
+      switch (ret)
+      {
+      case EAI_AGAIN:
+        continue;
+
+      case EAI_NONAME:
+      default:
+        break;
+      }
+    }
+    else
+    {
+      success= true;
+      break;
+    }
+  }
+
+  freeaddrinfo(addrinfo);
+
+  return success;
+}
+
+
+bool check_dns()
+{
+  if (lookup("exist.gearman.info") == false)
+  {
+    return false;
+  }
+
+  if (lookup("does_not_exist.gearman.info")) // This should fail, if it passes,...
+  {
+    return false;
+  }
+
+  return true;
+}
+
+} // namespace libtest
+
