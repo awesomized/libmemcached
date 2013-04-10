457e8cd000fa47f002c20af7a8227b7444ab1b42
Brian Aker <brian@tangent.org>
2013-04-10 02:51:13 -0400

    Fix warnings found by cppcheck.

--- clients/memrm.cc
+++ clients/memrm.cc
@@ -213,7 +213,7 @@ static void options_parse(int argc, char *argv[])
       opt_expire= (time_t)strtoll(optarg, (char **)NULL, 10);
       if (errno != 0)
       {
-        std::cerr << "Incorrect value passed to --expire: `" << optarg << "`" << std::cerr;
+        std::cerr << "Incorrect value passed to --expire: `" << optarg << "`" << std::endl;
         exit(EXIT_FAILURE);
       }
       break;
