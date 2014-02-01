212b9cda43dfdb1a6a5790a2365906e9887c678a
Brian Aker <brian@tangent.org>
2014-02-01 11:06:55 +0100

    Remove extra if

--- libmemcached/virtual_bucket.c
+++ libmemcached/virtual_bucket.c
@@ -109,14 +109,11 @@ uint32_t memcached_virtual_bucket_get(const memcached_st *self, uint32_t digest)
   {
     if (self->virtual_bucket)
     {
-      if (self->virtual_bucket)
-      {
-        uint32_t result= (uint32_t) (digest & (self->virtual_bucket->size -1));
-        return self->virtual_bucket->buckets[result].master;
-      }
-
-      return (uint32_t) (digest & (self->number_of_hosts -1));
+      uint32_t result= (uint32_t) (digest & (self->virtual_bucket->size -1));
+      return self->virtual_bucket->buckets[result].master;
     }
+
+    return (uint32_t) (digest & (self->number_of_hosts -1));
   }
 
   return 0;
