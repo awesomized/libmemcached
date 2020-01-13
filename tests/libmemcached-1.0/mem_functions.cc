bd8a989e9f0d0c115ae771732c9199589e3043fd
Michael Wallner <mike@php.net>
2020-01-13 14:56:35 +0100

    tests: fix kill(HUP) test

--- tests/libmemcached-1.0/mem_functions.cc
+++ tests/libmemcached-1.0/mem_functions.cc
@@ -5079,8 +5079,7 @@ test_return_t kill_HUP_TEST(memcached_st *original_memc)
                                         test_literal_param(__func__), // Keys
                                         test_literal_param(__func__), // Values
                                         0, 0);
-  test_compare(ret, memc);
-  test_compare(MEMCACHED_CONNECTION_FAILURE, memc);
+  test_compare(MEMCACHED_CONNECTION_FAILURE, ret);
 
   memcached_free(memc);
 
