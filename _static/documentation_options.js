3fc1f78f8a21346e8ddc81f5f97e49042cd3ac76
Michael Wallner <mike@php.net>
2020-02-20 12:47:39 +0100

    update docs

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,11 +1,11 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '',
+    VERSION: '1.0.99',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
-    HAS_SOURCE: true,
+    HAS_SOURCE: false,
     SOURCELINK_SUFFIX: '.txt',
     NAVIGATION_WITH_KEYS: false
 };
