4d48a64682e4921bda97f1e5ae30321f0cb2b15b
Michael Wallner <mike@php.net>
2020-09-30 12:00:35 +0200

    testing: fix ASAN_OPTIONS

--- test/main.cpp
+++ test/main.cpp
@@ -1,24 +1,8 @@
 #define CATCH_CONFIG_RUNNER
 #include "lib/catch.hpp"
-#include "mem_config.h"
-#include <cstdlib>
-
-#if HAVE_SETENV
-# define SET_ENV(n, k, v) setenv(k, v, 0);
-#else // !HAVE_SETENV
-# define SET_ENV(n, k, v) static char n ## _env[] = k "=" v; putenv(n ## _env)
-#endif
+#include "setup.hpp"
 
 int main(int argc, char *argv[]) {
-
-#if HAVE_ASAN
-  SET_ENV(asan, "ASAN_OPTIONS", "halt_on_error=0")
-#endif
-
-#if LIBMEMCACHED_WITH_SASL_SUPPORT
-  SET_ENV(sasl_pwdb, "MEMCACHED_SASL_PWDB", LIBMEMCACHED_WITH_SASL_PWDB);
-  SET_ENV(sasl_conf, "SASL_CONF_PATH", LIBMEMCACHED_WITH_SASL_CONF);
-#endif
-
+  setup(argc, &argv);
   return Catch::Session().run(argc, argv);
 }
