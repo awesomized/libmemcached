2dd79b61ec7944b82ec6b576cc081603ec526049
Michael Wallner <mike@php.net>
2020-10-13 11:56:03 +0200

    testing: parser

--- test/lib/env.hpp
+++ test/lib/env.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include "mem_config.h"
 
 #include <cstdlib>
--- test/tests/memcached/parser.cpp
+++ test/tests/memcached/parser.cpp
@@ -1,6 +1,9 @@
 #include "test/lib/common.hpp"
 #include "test/lib/env.hpp"
 
+#include "libmemcached/common.h"
+#include "test/fixtures/parser.hpp"
+
 TEST_CASE("memcached_parser") {
   SECTION("fail: null string") {
     REQUIRE_FALSE(memcached(nullptr, 123));
@@ -29,6 +32,20 @@ TEST_CASE("memcached_parser") {
     SECTION("fail: extra quotes") {
       SET_ENV(fail_extra_quotes, "LIBMEMCACHED", "\"--server=localhost\"");
       REQUIRE_FALSE(memcached(nullptr, 0));
+    }
+  }
+
+  SECTION("fixtures") {
+    for (const auto &test : tests) {
+      DYNAMIC_SECTION(test.name) {
+        for (size_t i = 0; i < test.ntests; ++i) {
+          auto &tc = test.tests[i];
+          DYNAMIC_SECTION(tc.option.c_str) {
+            MemcachedPtr memc(memcached(tc.option.c_str, tc.option.size));
+            test.check(*memc, tc.result);
+          }
+        }
+      }
     }
   }
 }
