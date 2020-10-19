3cddec3d3b13326ee72b6376081254e32ec1483d
Michael Wallner <mike@php.net>
2020-10-19 14:18:09 +0200

    testing: fix lp1021819

--- test/tests/memcached/regression/lp_001-021-819.cpp
+++ test/tests/memcached/regression/lp_001-021-819.cpp
@@ -1,13 +1,15 @@
 #include "test/lib/common.hpp"
+#include "test/lib/MemcachedCluster.hpp"
 
 TEST_CASE("memcached_regression_lp1021819") {
-  MemcachedPtr memc;
+  auto test = MemcachedCluster::network();
+  auto memc = &test.memc;
 
-  REQUIRE(MEMCACHED_SUCCESS == memcached_behavior_set(*memc, MEMCACHED_BEHAVIOR_SND_TIMEOUT, 2'000'000));
-  REQUIRE(MEMCACHED_SUCCESS == memcached_behavior_set(*memc, MEMCACHED_BEHAVIOR_RCV_TIMEOUT, 2'000'000));
+  CHECK(MEMCACHED_SUCCESS == memcached_behavior_set(memc, MEMCACHED_BEHAVIOR_SND_TIMEOUT, 2'000'000));
+  CHECK(MEMCACHED_SUCCESS == memcached_behavior_set(memc, MEMCACHED_BEHAVIOR_RCV_TIMEOUT, 2'000'000));
 
   memcached_return_t rc;
-  Malloced val(memcached_get(*memc, S("not-found"), nullptr, nullptr, &rc));
+  Malloced val(memcached_get(memc, S("not-found"), nullptr, nullptr, &rc));
   REQUIRE(MEMCACHED_NOTFOUND == rc);
   REQUIRE_FALSE(*val);
 }
