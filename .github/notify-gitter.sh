2902291b9589d17e55d2397959c8cff9772b38a3
Michael Wallner <mike@php.net>
2020-12-21 09:44:26 +0100

    ci: gh-actions: consolidate gitter notify

---
+++ .github/notify-gitter.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+GITTER=$1
+
+REPO=${GITHUB_REPOSITORY}
+
+REF=$(basename ${GITHUB_REF})
+REF_URL=https://github.com/${REPO}/commits/${REF}
+
+BUILD_URL=https://github.com/${REPO}/actions/runs/${GITHUB_RUN_ID}
+BUILD_ENV=${ImageOS}/${CC:-${CC_VND}-${CC_VER}}
+
+case "$2" in
+1|true)
+	level=info
+	status=success
+	;;
+*)
+	level=error
+	status=failure
+	;;
+esac
+message="Github [${REPO}](${REF_URL}) (${REF}) [${status}](${BUILD_URL}) (${BUILD_ENV})"
+
+curl -sS "${GITTER}" --data-urlencode "level=${level}" --data-urlencode "message=${message}"
+
