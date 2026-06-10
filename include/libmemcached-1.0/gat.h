8017841fa5f80a0962f902bf9c652e98ade6f3b0
Peter (Stig) Edwards <thatsafunnyname@gmail.com>
2026-06-10 17:02:21 +0100

    Create gat.h

---
+++ include/libmemcached-1.0/gat.h
@@ -0,0 +1,33 @@
+/*
+    +--------------------------------------------------------------------+
+    | libmemcached-awesome - C/C++ Client Library for memcached          |
+    +--------------------------------------------------------------------+
+    | Redistribution and use in source and binary forms, with or without |
+    | modification, are permitted under the terms of the BSD license.    |
+    | You should have received a copy of the license in a bundled file   |
+    | named LICENSE; in case you did not receive a copy you can review   |
+    | the terms online at: https://opensource.org/licenses/BSD-3-Clause  |
+    +--------------------------------------------------------------------+
+    | Copyright (c) 2006-2014 Brian Aker   https://datadifferential.com/ |
+    | Copyright (c) 2020-2021 Michael Wallner        https://awesome.co/ |
+    +--------------------------------------------------------------------+
+*/
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+LIBMEMCACHED_API
+char *memcached_gat(memcached_st *ptr, const char *key, size_t key_length, time_t expiration,
+                    size_t *value_length, uint32_t *flags, memcached_return_t *error);
+
+LIBMEMCACHED_API
+char *memcached_gat_by_key(memcached_st *ptr, const char *group_key, size_t group_key_length,
+                           const char *key, size_t key_length, time_t expiration,
+                           size_t *value_length, uint32_t *flags, memcached_return_t *error);
+
+#ifdef __cplusplus
+}
+#endif
